cce5037739072e5d1ddd0f2c20f96131de64c6a0
Tran Le Hai Duong <duong.tlh.909@aptechlearning.edu.vn>
2022-06-22 21:09:26 +0700

    second commit

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,44 +1,53 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { COMMENTS } from '../shared/comments'
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-class DishDetail extends Component {
-    render() {
-        if (!this.props.dish) return <div></div>;
-        return (
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    <Card>
-                        <CardImg width="100%" src={this.props.dish.image} alt={this.props.dish.name} />
-                        <CardBody>
-                            <CardTitle>{this.props.dish.name}</CardTitle>
-                            <CardText>{this.props.dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                </div>
-                <div className="col-12 col-md-6 m-1">
-                    <div>
-                        <h4>Comments</h4>
-                        {
-                            this.props.comments.map(comment => {
-                                const date = new Date(comment.date)
-                                return (
-                                    <div key={comment.id}>
-                                        <CardText style={{
-                                            margin: '15px 0px'
-                                        }}>{comment.comment}</CardText>
-                                        <CardText style={{
-                                            margin: '15px 0px'
-                                        }}>-- {comment.author}, {months[date.getMonth()]} {date.getDate() >= 10 ? date.getDate() : `0${date.getDate()}`}, {date.getFullYear()}</CardText>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                </div>
-            </div>
-        );
-    }
+function RenderDish({ dish }) {
+    return (
+        <div className="col-12 col-md-5 m-1">
+            <Card>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    )
+}
 
+function RenderComments({ comments }) {
+    return (
+        <div className="col-12 col-md-6 m-1">
+            <div>
+                <h4>Comments</h4>
+                {
+                    comments.map(comment => {
+                        const date = new Date(comment.date)
+                        return (
+                            <div key={comment.id}>
+                                <CardText style={{
+                                    margin: '15px 0px'
+                                }}>{comment.comment}</CardText>
+                                <CardText style={{
+                                    margin: '15px 0px'
+                                }}>-- {comment.author}, {months[date.getMonth()]} {date.getDate() >= 10 ? date.getDate() : `0${date.getDate()}`}, {date.getFullYear()}</CardText>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+const DishDetail = (props) => {
+    if (!props.dish) return <div></div>;
+    return (
+        <div className="row">
+            <RenderDish dish={props.dish} />
+            <RenderComments comments={COMMENTS.filter(c => c.dishId === props.dish.id)} />
+        </div>
+    );
 }
 
 export default DishDetail
