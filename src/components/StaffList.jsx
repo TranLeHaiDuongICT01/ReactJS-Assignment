6af8d9765b09a407f27168994cd42d0fe6b8ef12
Tran Le Hai Duong <duong.tlh.909@aptechlearning.edu.vn>
2022-06-28 09:55:17 +0700

    update

--- src/components/StaffList.jsx
+++ src/components/StaffList.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Card, CardText, CardImg, Input, Button, Modal, ModalHeader, ModalBody, Form, Row, Label, Col } from 'reactstrap';
-import { Control, LocalForm } from "react-redux-form";
-const { text: Text, select: Select } = Control;
+import { Card, CardText, CardImg, Input, Button, Form } from 'reactstrap';
+import ModalAddStaff from "./ModalAddStaff";
 const Staffs = ({ staffs }) => {
     return (
         staffs?.map(staff => (
@@ -77,7 +76,7 @@ class StaffList extends Component {
             <React.Fragment>
                 <div className="row m-auto container p-0">
                     <div className="col-12 col-lg-4 mt-4" style={{ display: 'flex', gap: '20px' }}>
-                        <h3 style={{ width: '100%' }}>Nhân Viên</h3>
+                        <h3 className="staff-list-title">Nhân Viên</h3>
                         <Button className="btn-form" onClick={this.toggleModal}>Add</Button>
                     </div>
                     <Form className="col-12 col-lg-4 mt-4" style={{ display: 'flex', gap: '20px' }} onSubmit={this.handleSearch}>
@@ -98,112 +97,13 @@ class StaffList extends Component {
                         <Staffs staffs={this.props.staffs} />
                     </div>
                 </div>
-                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal} unmountOnClose={false}>
-                    <ModalHeader toggle={this.toggleModal}>Thêm Nhân Viên</ModalHeader>
-                    <ModalBody>
-                        <LocalForm onSubmit={(values) => this.handleSubmit(values)} className='form'>
-                            <Row className="form-group">
-                                <Label htmlFor="name" md={5}>Tên</Label>
-                                <Col md={7}>
-                                    <Text
-                                        className="form-control"
-                                        model=".name"
-                                        id="name"
-                                        name="name"
-                                    />
-                                </Col>
-                            </Row>
+                <ModalAddStaff 
+                isModalOpen={this.state.isModalOpen}
+                toggleModal={this.toggleModal}
+                handleSubmit={this.handleSubmit}
+                departments={this.props.departments}
+                />
 
-                            <Row className="form-group">
-                                <Label htmlFor="doB" md={5}>Ngày sinh</Label>
-                                <Col md={7}>
-                                    <Text
-                                        type="date"
-                                        className="form-control"
-                                        model=".doB"
-                                        id="doB"
-                                        name="doB"
-                                    />
-                                </Col>
-                            </Row>
-
-                            <Row className="form-group">
-                                <Label htmlFor="startDate" md={5}>Ngày vào công ty</Label>
-                                <Col md={7}>
-                                    <Text
-                                        type="date"
-                                        className="form-control"
-                                        model=".startDate"
-                                        id="startDate"
-                                        name="startDate"
-                                    />
-                                </Col>
-                            </Row>
-
-                            <Row className="form-group">
-                                <Label htmlFor="department" md={5}>Phòng ban</Label>
-                                <Col md={7}>
-                                    <Select
-                                        className="form-control form-select"
-                                        model=".department"
-                                        id="department"
-                                        name="department"
-                                        defaultValue="Sale"
-                                    >
-                                        {this?.props?.departments?.map(department => (
-                                            <option key={department.id} value={department.name}>{department.name}</option>
-                                        ))}
-                                    </Select>
-                                </Col>
-                            </Row>
-
-                            <Row className="form-group">
-                                <Label htmlFor="salaryScale" md={5}>Hệ số lương</Label>
-                                <Col md={7}>
-                                    <Text
-                                        type="number"
-                                        className="form-control"
-                                        model=".salaryScale"
-                                        id="salaryScale"
-                                        name="salaryScale"
-                                    />
-                                </Col>
-                            </Row>
-
-                            <Row className="form-group">
-                                <Label htmlFor="annualLeave" md={5}>Số ngày nghỉ còn lại</Label>
-                                <Col md={7}>
-                                    <Text
-                                        type="number"
-                                        className="form-control"
-                                        model=".annualLeave"
-                                        id="annualLeave"
-                                        name="annualLeave"
-                                    />
-                                </Col>
-                            </Row>
-
-                            <Row className="form-group">
-                                <Label htmlFor="overTime" md={5}>Số ngày đã làm thêm</Label>
-                                <Col md={7}>
-                                    <Text
-                                        type="number"
-                                        className="form-control"
-                                        model=".overTime"
-                                        id="overTime"
-                                        name="overTime"
-                                    />
-                                </Col>
-                            </Row>
-
-                            <Row className='form-group mt-3 m-auto'>
-                                <Col>
-                                    <Button type='submit' color='primary'>Thêm</Button>
-                                </Col>
-                            </Row>
-                        </LocalForm>
-                    </ModalBody>
-                </Modal>
             </React.Fragment>
         );
     }
